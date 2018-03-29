107ebfd6dfb5335fdfe3e19cb2fdcf668ee4cb39
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-03-29 13:30:17 -0700

    relax page functions DONT TOUCH CODE

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,20 @@
+// firebase intitialization for productivity page
+// var config = {
+//     apiKey: "AIzaSyBPmro-Lmwer67ByMa3D-64qu7U-CsbT5I",
+//     authDomain: "diaapp-199504.firebaseapp.com",
+//     databaseURL: "https://diaapp-199504.firebaseio.com",
+//     projectId: "diaapp-199504",
+//     storageBucket: "diaapp-199504.appspot.com",
+//     messagingSenderId: "827656289467"
+//   };
+//   firebase.initializeApp(config);
+// var database = 
 //on click event for search submit button on relax page. button submission triggers search on giphy and youtube and populates those divs after request is completed
-$("body").on("click", "INSERT SUBMIT BUTTON", function () {
-    e.preventDefault();
-    var search = $("INSERT INPUT VALUE").val().trim();
-    console.log(search);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=PXKd89OxBUGSE1Ptn1uQAXeN3hNlg5uc&limit=10";
+$("#submit-input").on("click", function (event) {
+    event.preventDefault();
+    var searchTerm = $("#search-input").val().trim();
+    console.log(searchTerm);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=PXKd89OxBUGSE1Ptn1uQAXeN3hNlg5uc&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -25,9 +36,9 @@ $("body").on("click", "INSERT SUBMIT BUTTON", function () {
             gifHolder.attr("data-still", stillUrl);
             gifHolder.attr("data-state", "still");
             gifDiv.prepend(gifHolder);
-            gifDiv.prepend("<div class='title-rating panel panel-default'> <strong>Title: </strong>"+gifTitle+"  <strong>Rating: </strong>" + gifRating + "</div>");
+            gifDiv.prepend("<div class='title-rating panel panel-default'> <strong>Title: </strong>" + gifTitle + "  <strong>Rating: </strong>" + gifRating + "</div>");
             //gifDiv.append("<button class='btn btn-default download' id='download'><a href=" + animateUrl + " target='blank' download>Download GIF</button");
-            $("INSERT ID FROM RELAX PAGE").prepend(gifDiv);
+            $("#gifs").prepend(gifDiv);
         }
 
         $("body").on("click", "img", function () {
@@ -42,50 +53,46 @@ $("body").on("click", "INSERT SUBMIT BUTTON", function () {
                 $(this).attr("data-state", "still");
             }
             then();
-        })
 
+        })
     })
-    
     //youtube stuff
     function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
-    $(function () {
-    $("form").on("submit", function (e) {
-    e.preventDefault();
-        // prepare the request
         var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent(search.val()).replace(/%20/g, "+"),
+            q: encodeURIComponent(searchTerm.replace(/%20/g, "+")),
             maxResults: 10,
             order: "viewCount",
         });
         // execute the request
         request.execute(function (response) {
             var results = response.result;
-            $("INSERT ID FROM HTML FOR DIV WITH VIDEOS").html("");
+            $("#youtube-videos").html("");
             $.each(results.items, function (index, item) {
                 $.get("tpl/item.html", function (data) {
-                    $("INSERT ID FROM HTML FOR DIV WITH VIDEOS").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
+                    $("#youtube-videos").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
                 });
             });
             resetVideoHeight();
         });
-    });
 
-    $(window).on("resize", resetVideoHeight);
-});
 
-function resetVideoHeight() {
-    $(".video").css("height", $("#results").width() * 9 / 16);
-}
+        $(window).on("resize", resetVideoHeight);
+    })
 
-function init() {
-    gapi.client.setApiKey("AIzaSyCNBL_nvAFef_wJLbPQ9yIKBhAN341NeUo");
-    gapi.client.load("youtube", "v3", function () {
-        // yt api is ready
-    });
-}
-});
+    function resetVideoHeight() {
+        $(".video").css("height", $("#results").width() * 9 / 16);
+    }
+
+    function init() {
+        gapi.client.setApiKey("AIzaSyCNBL_nvAFef_wJLbPQ9yIKBhAN341NeUo");
+        gapi.client.load("youtube", "v3", function () {
+            // yt api is ready
+        });
+    }
+
+
 
 //youtube stuff
 // function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
