b21c4e77c4f630d54e1f3f6b09bc21da843671bf
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-02 22:42:44 -0700

    fixed path for youtube videos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,7 +58,7 @@ $("#submit-input").on("click", function (event) {
             var results = response.result;
             $("#youtube-videos").html("");
             $.each(results.items, function (index, item) {
-                $.get("tpl/item.html", function (data) {
+                $.get("../tpl/item.html", function (data) {
                     $("#youtube-videos").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
                 });
             });
